f28a753d4f5ad554e3cd1fd14ab72ef49b0d5f92
Alex <quixoticopposites@gmail.com>
2020-07-19 12:08:05 +0900

    Added game canvas class to encapsulate fg/bg canvas rendering; added linked list based queue for render queue

--- client/game_canvas.ts
+++ client/game_canvas.ts
@@ -1,34 +1,59 @@
-// CANVAS
-interface RenderItem {
+import { Queue } from "../shared/linked_list";
 
+export interface RenderContext {
+  start: DOMHighResTimeStamp;
+  end: DOMHighResTimeStamp;
+}
+
+export interface RenderItem {
+  ctx: RenderContext,
+  cb: (ts: DOMHighResTimeStamp, ctx: RenderContext) => void,
 }
 
 export class GameCanvas extends HTMLDivElement {
-  constructor() {
+  public constructor() {
     super();
-    this.canvasBG.setAttribute("class", "canvas canvas-bg");
-    this.canvasFG.setAttribute("class", "canvas canvas-fg");
-    this.appendChild(this.canvasBG);
-    this.appendChild(this.canvasFG);
+    this._canvasBG.setAttribute("class", "canvas canvas-bg");
+    this._canvasFG.setAttribute("class", "canvas canvas-fg");
+    this.appendChild(this._canvasBG);
+    this.appendChild(this._canvasFG);
   }
-
-  public startRender() {
-    window.requestAnimationFrame();
-  }
-
-  private renderFrame() {
-    let renderItem = this.renderQueue.pop();
-    if (renderItem !== undefined) {
-
+  
+  public stop() {
+    while (this._animFrameCBs.length > 0) {
+      const cbId = this._animFrameCBs.pop();
+      if (cbId == undefined) break; // probably not necessary due to evloop js
+      window.cancelAnimationFrame(cbId);
     }
-    const cbId = window.requestAnimationFrame();
-    this.animFrameCBs.
   }
 
-  private canvasBG = new HTMLCanvasElement();
-  private canvasFG = new HTMLCanvasElement();
-  private ctxBG = this.canvasBG.getContext("2d");
-  private ctxFG = this.canvasFG.getContext("2d");
-  private renderQueue = new Array<RenderItem>();
-  private animFrameCBs = new Array<number>();
-} 
+  public start() {
+    const cb: FrameRequestCallback = this.renderFrame.bind(this);
+    const cbId = window.requestAnimationFrame(cb);
+    this._animFrameCBs.push(cbId);
+  }
+  
+  // Public getters for protected/private properties
+  public get renderQueue() { return this._renderQueue; }
+  public get getBgContext() { return this._ctxBG; }
+  public get getFgContext() { return this._ctxFG; }
+
+  // Protected members
+  protected _canvasBG = new HTMLCanvasElement();
+  protected _canvasFG = new HTMLCanvasElement();
+  protected _ctxBG = this._canvasBG.getContext("2d");
+  protected _ctxFG = this._canvasFG.getContext("2d");
+  protected _renderQueue = new Queue<RenderItem>();
+
+  // Private members
+  private _animFrameCBs = new Array<number>();
+  private renderFrame(ts: DOMHighResTimeStamp) {
+    let renderItem: RenderItem;
+    if (renderItem = this._renderQueue.dequeue()) {
+      renderItem.cb(ts, renderItem.ctx);
+    }
+    const cb: FrameRequestCallback = this.renderFrame.bind(this);
+    const cbId = window.requestAnimationFrame(cb);
+    this._animFrameCBs.push(cbId);
+  }
+}
---
+++ shared/linked_list.ts
@@ -0,0 +1,66 @@
+interface LinkedItem<T> {
+  next: LinkedItem<T> | null;
+  data: T;
+}
+
+export class LinkedList<T> {
+  public head: LinkedItem<T>;
+  public tail: LinkedItem<T>;
+  public length: number;
+
+  public append(item: LinkedItem<T>): number {
+    this.tail.next = item;
+    this.length += 1;
+    return this.length;
+  }
+
+  public insert(item: LinkedItem<T>, at: number) {
+    let prevNode: LinkedItem<T> = null;
+    let node: LinkedItem<T> = this.head;
+    // traverse linked list
+    for (let i = 0; i < Math.min(at, this.length); ++i) {
+      prevNode = node;
+      node = node.next;
+    }
+    if (prevNode === null) {
+      this.head = item;
+    } else {
+      prevNode.next = item;
+    }
+    item.next = node;
+  }
+
+  public remove(at: number): LinkedItem<T> {
+    let prevNode: LinkedItem<T> = null;
+    let node: LinkedItem<T> = this.head;
+    // traverse linked list
+    for (let i = 0; i < Math.min(at, this.length); ++i) {
+      prevNode = node;
+      node = node.next;
+    }
+    if (prevNode === null) {
+      this.head = node.next;
+    } else {
+      prevNode.next = node.next;
+    }
+    node.next = null;
+    return node;
+  }
+}
+
+export class Queue<T> extends LinkedList<T> {
+  get front(): T { return this.head.data };
+  get back(): T { return this.tail.data };
+
+  public dequeue(): T {
+    const item = this.head;
+    this.head = item.next;
+    delete item.next;
+    this.length -= 1;
+    return item.data;
+  }
+
+  public enqueue(data: T): number {
+    return this.append({ next: null, data });
+  }
+}
